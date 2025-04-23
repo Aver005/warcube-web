4e6fa81ce524fd066cfe4adec8900fa13e3a77f2
artaveryan <artaveryan@rtatex.ru>
2025-04-23 13:53:59 +0300

    fix: (Cline) Make socket events with enum

--- client/src/network/NetworkManager.ts
+++ client/src/network/NetworkManager.ts
@@ -1,4 +1,5 @@
 import io, { Socket } from "socket.io-client";
+import { SocketEvent } from "../types/SocketEvents";
 import MainScene from "../main-scene";
 import { PlayerData, PlayerMovementData, PlayerShootEvent } from "../types/Player";
 import { PlayerDeadEvent } from "../types/events/player-events";
@@ -41,78 +42,78 @@ export class NetworkManager
 
     private setupListeners()
     {
-        this.socket.on('connect', () => useNetworkStore.getState().setSocket(this.socket));
+this.socket.on(SocketEvent.CONNECT, () => useNetworkStore.getState().setSocket(this.socket));
 
-        this.socket.on('init', (data: InitEvent) =>
+this.socket.on(SocketEvent.INIT, (data: InitEvent) =>
+{
+    // Delegate item spawning to GameMap
+    data.itemsOnGround.forEach(item => this.match.getGameMap().spawnItem(item));
+
+    // Delegate player creation to GameMap
+    Object.entries(data.players).forEach(([id, playerData]) =>
+    {
+        const player = this.match.getGameMap().addPlayer(playerData);
+        if (id === this.socket.id)
         {
-            // Delegate item spawning to GameMap
-            data.itemsOnGround.forEach(item => this.match.getGameMap().spawnItem(item));
+            this.scene.setupCamera();
+        }
+    });
+});
 
-            // Delegate player creation to GameMap
-            Object.entries(data.players).forEach(([id, playerData]) =>
-            {
-                const player = this.match.getGameMap().addPlayer(playerData);
-                if (id === this.socket.id)
-                {
-                    this.scene.setupCamera();
-                }
-            });
-        });
+this.socket.on(SocketEvent.NEW_PLAYER, (playerData: PlayerData) =>
+{
+    this.match.getGameMap().addPlayer(playerData);
+});
 
-        this.socket.on('newPlayer', (playerData: PlayerData) =>
-        {
-            this.match.getGameMap().addPlayer(playerData);
-        });
+this.socket.on(SocketEvent.PLAYER_DISCONNECTED, (playerId: string) =>
+{
+    this.match.getGameMap().removePlayer(playerId);
+});
 
-        this.socket.on('playerDisconnected', (playerId: string) =>
-        {
-            this.match.getGameMap().removePlayer(playerId);
-        });
+this.socket.on(SocketEvent.PLAYER_MOVED, (data: PlayerMovementData) =>
+{
+    this.match.getGameMap().updatePlayerPosition(data);
+});
 
-        this.socket.on('playerMoved', (data: PlayerMovementData) =>
-        {
-            this.match.getGameMap().updatePlayerPosition(data);
-        });
+this.socket.on(SocketEvent.PLAYER_SHOOT, (data: PlayerShootEvent) =>
+{
+    this.match.getGameMap().addBullet(data);
+});
 
-        this.socket.on('playerShoot', (data: PlayerShootEvent) =>
-        {
-            this.match.getGameMap().addBullet(data);
-        });
+this.socket.on(SocketEvent.PLAYER_RELOAD, (playerId: string) =>
+{
+    this.match.getGameMap().setPlayerReloading(playerId, true);
+});
 
-        this.socket.on('playerReload', (playerId: string) =>
-        {
-            this.match.getGameMap().setPlayerReloading(playerId, true);
-        });
+this.socket.on(SocketEvent.PLAYER_RELOAD_COMPLETE, (playerId: string) =>
+{
+    this.match.getGameMap().setPlayerReloading(playerId, false);
+});
 
-        this.socket.on('playerReloadComplete', (playerId: string) =>
-        {
-            this.match.getGameMap().setPlayerReloading(playerId, false);
-        });
+this.socket.on(SocketEvent.PLAYER_DEAD, (data: PlayerDeadEvent) =>
+{
+    useMapDataStore.getState().newAction(
+        `${data.name} was killed by ${data.killerName}`
+    );
 
-        this.socket.on('playerDead', (data: PlayerDeadEvent) =>
-        {
-            useMapDataStore.getState().newAction(
-                `${data.name} was killed by ${data.killerName}`
-            );
+    if (data.killerId === this.socket.id)
+    {
+        useGameStore.getState().setKills(useGameStore.getState().kills + 1);
+    } 
+    else
+    {
+        useGameStore.getState().setDeaths(useGameStore.getState().deaths + 1);
+    }
+});
 
-            if (data.killerId === this.socket.id)
-            {
-                useGameStore.getState().setKills(useGameStore.getState().kills + 1);
-            } 
-            else
-            {
-                useGameStore.getState().setDeaths(useGameStore.getState().deaths + 1);
-            }
-        });
+this.socket.on(SocketEvent.PLAYER_PICKUP_ITEM, (itemId: number) =>
+{
+    this.match.getGameMap().removeItem(itemId);
+});
 
-        this.socket.on('playerPickupItem', (itemId: number) =>
-        {
-            this.match.getGameMap().removeItem(itemId);
-        });
-
-        this.socket.on('disconnect', () =>
-        {
-            this.match.destroy();
-        });
+this.socket.on(SocketEvent.DISCONNECT, () =>
+{
+    this.match.destroy();
+});
     }
 }
